7feec8a682f3a688141ff8159f7764214e9cf6d0
Marcus <marcuz1111@gmail.com>
2013-01-04 16:57:57 +0100

    Update modules/tables/rows.php
    Fixed the vehicle result links // Before the change I did when you pressed a vehicle link you got to the wrong vehicle's info.

--- modules/tables/rows.php
+++ modules/tables/rows.php
@@ -287,7 +287,7 @@ function row_vehicle($row, $chbox){
 	}	
 	
 	$tablerow = "<tr>".$chbox."
-		<td align=\"center\" class=\"gear_preview\" ><a href=\"admin.php?view=info&show=4&id=".$row['id']."\">".$row['id']."</a></td>
+		<td align=\"center\" class=\"gear_preview\" ><a href=\"admin.php?view=info&show=4&id=".$row['instance_vehicle_id']."\">".$row['id']."</a></td>
 		<td align=\"center\" class=\"gear_preview\" ><a href=\"admin.php?view=info&show=4&id=".$row['id']."\">".$row['class_name']."</a></td>			
 		<td align=\"center\" class=\"gear_preview\" ><a href=\"admin.php?view=info&show=4&id=".$row['id']."\">".$row['id']."</a></td>
 		<td align=\"center\" class=\"gear_preview\" style=\"background-color: rgba(100,".round((255/100)*(100 - ($row['damage']*100))).",0,0.8);\">".substr($row['damage'], 0, 6)."</td>
