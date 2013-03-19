62034eff4492eab37e8918352e65500a51feff8a
R4Z0R49 <R4Z0R49@gmail.com>
2013-03-19 01:24:16 -0500

    Updated queries for map 4 (Vehicles)

--- modules/maps/4.php
+++ modules/maps/4.php
@@ -1,6 +1,9 @@
 <?php
 
-$res = $db->GetAll("SELECT iv.*, v.class_name, oc.Type FROM instance_vehicle iv inner join  world_vehicle wv on iv.world_vehicle_id = wv.id inner join vehicle v on wv.vehicle_id = v.id inner join object_classes oc on oc.Classname = v.class_name WHERE iv.instance_id = ? AND wv.world_id = ?", array($iid, $world));
+include ('queries.php');
+$query = $map4[0];
+$binds = $map4[1];
+$res = $db->GetAll($query, $binds);
 $markers = markers_vehicle($res, $map);
 
 ?>
--- queries.php
+++ queries.php
@@ -24,6 +24,7 @@ switch($sql)
 		$info6 = array("SELECT id.*,d.class_name,p.name,p.unique_id player_unique_id from instance_deployable id JOIN deployable d on d.id = id.deployable_id JOIN survivor s ON s.id = id.owner_id JOIN profile p on p.unique_id = s.unique_id WHERE id.id = ? and instance_id = ? LIMIT 1", array($_GET["id"], $iid) );
 	//Maps
 		$map0 = array("select s.id, p.*, 'Player' as type, s.worldspace as worldspace, s.survival_time as survival_time, s.model as model, s.survivor_kills as survivor_kills, s.zombie_kills as zombie_kills, s.bandit_kills as bandit_kills, s.is_dead as is_dead, s.unique_id as unique_id from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 0 and s.world_id = ? and last_updated > now() - interval 1 minute", $world);
+		$map4 = array ("SELECT iv.*, v.class_name, oc.Type FROM instance_vehicle iv inner join  world_vehicle wv on iv.world_vehicle_id = wv.id inner join vehicle v on wv.vehicle_id = v.id inner join object_classes oc on oc.Classname = v.class_name WHERE iv.instance_id = ? AND wv.world_id = ?", array($iid, $world));
 		$map8_players = array("select s.id, p.*, 'Player' as type, s.worldspace as worldspace, s.survival_time as survival_time, s.model as model, s.survivor_kills as survivor_kills, s.zombie_kills as zombie_kills, s.bandit_kills as bandit_kills, s.is_dead as is_dead, s.unique_id as unique_id from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 0 and s.world_id = ? and last_updated > now() - interval 1 minute", $world);
 		$map8_vehicles = array("SELECT world_vehicle.vehicle_id, vehicle.class_name, object_classes.Type, instance_vehicle.*, instance.world_id FROM `world_vehicle`, `vehicle`, `object_classes`, `instance_vehicle`, `instance` WHERE vehicle.id = world_vehicle.vehicle_id AND instance_vehicle.world_vehicle_id = world_vehicle.id AND instance_vehicle.instance_id = ? AND world_vehicle.world_id = instance.world_id AND object_classes.classname = vehicle.class_name", $iid);
 		$map8_objects = array("select id.id,id.unique_id as idid,id.worldspace,id.inventory,id.last_updated,oc.Classname,oc.Type,p.name from instance_deployable id inner join deployable d on id.deployable_id = d.id inner join object_classes oc on d.class_name = oc.classname join survivor s on s.id = id.owner_id join profile p on p.unique_id = s.unique_id where d.class_name in ('Sandbag1_DZ', 'TrapBear', 'Hedgehog_DZ', 'Wire_cat1', 'TentStorage') and id.instance_id = ?", $iid);
@@ -92,6 +93,17 @@ from player_data, character_data
 where player_data.PlayerUID = character_data.PlayerUID 
 and character_data.Alive = 1 
 and character_data.last_updated >= NOW() - INTERVAL 1 minute");
+		$map4 = array("SELECT object_classes.*,
+		object_data.ObjectID as id,
+		object_data.ObjectUID as uid,
+		object_data.Classname as class_name,
+		object_data.Worldspace as worldspace,
+		object_data.Damage as damage,
+		object_data.last_updated,
+		object_data.Fuel as fuel
+		FROM object_classes, object_data as object_data  
+		where object_data.Classname = object_classes.Classname 
+		and object_data.Instance =  '" . $iid . "' and CharacterID = '0'");
 		$map8_players = array("select player_data.playerName as name, player_data.playerUID, 
 character_data.PlayerUID as unique_id, 
 character_data.CharacterID as id,
