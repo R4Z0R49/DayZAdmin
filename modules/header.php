ec16f10e020ebee945dec2f83b6841844b5926d6
Sharkiller <sharkale3101@hotmail.com>
2013-04-07 13:15:06 -0300

    Ups typo

--- modules/header.php
+++ modules/header.php
@@ -2,6 +2,7 @@
 if (isset($_SESSION['user_id'])) {
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html;" />
 <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
