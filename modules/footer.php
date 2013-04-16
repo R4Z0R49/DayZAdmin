bcc4e450f6d794fbb08f1b50daa102b441a9e042
Marcus <marcuz1111@gmail.com>
2013-04-16 19:47:44 +0300

    Footer fix, fixed a few typos too.

--- modules/footer.php
+++ modules/footer.php
@@ -1,29 +1,10 @@
-<?php
-if (isset($_SESSION['user_id']))
-{
-?>
-</div>
-<!--  end content -->
-</div>
-<!--  end content-outer........................................................END -->
-
-<div class="clear">&nbsp;</div>
-<!-- start footer -->         
-<div id="footer">
+<?php 
+$footer="       
+<div id='footer'>
 	<!--  start footer-left -->
-	<div id="footer-left">
-	<?php echo $sitename ?> Panel &copy; Copyright 2006-2013 Admin-panel. Creator katzsmile, Redesigned by R4Z0R49/Wiley. Theme Redesigned By Marcuz.</div>
+	<div id='footer-left'>
+	<?php echo $sitename ?> Panel &copy; Copyright 2006-2013 Admin-panel. Creator katzsmile. Redesigned by R4Z0R49/Wriley. Latest theme redesigned by Marcuz.</div>
 	<!--  end footer-left -->
-	<div class="clear">&nbsp;</div>
-</div>
-<!-- end footer -->
- 
-</body>
-</html>
-<?php
-}
-else
-{
-	header('Location: admin.php');
-}
+	<div class='clear'>&nbsp;</div>
+</div>";
 ?>
