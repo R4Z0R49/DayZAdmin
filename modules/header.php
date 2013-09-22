24a573c19ca98271ccb0873fa2aca17eaa79aeb5
Marcuz <marcuz1111@gmail.com>
2013-09-22 15:49:59 +0200

    Navbar fix for firefox

--- modules/header.php
+++ modules/header.php
@@ -13,19 +13,18 @@ if(isset($_GET["view"]) && $_GET["view"] == "table" && isset($_GET["show"]) && $
 	<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
 	<link rel="stylesheet" href="css/watch.css" />	
 	<link rel="stylesheet" href="css/flexcrollstyles.css" />	
-	<script language="javascript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>	
 	<script src="js/flexcroll.js" type="text/javascript"></script>
 	<script type="text/javascript" src="js/jquery.fullscreen.js"></script>
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
 	<!-- New design (Bootstrap - font-awesome) -->
 	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
 	<link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-	<script src="http://code.jquery.com/jquery.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
 	
 	<script src="js/modalpopup.js" type="text/javascript"></script>
 	<?php if($enableclock) { ?>
-	<script type="text/javascript" src="js/jquery/jquery-1.4.1.min.js"></script>
 	<script type="text/javascript" src="js/jquery.jclock.js"></script>
 	<script type="text/javascript">
 		$(function($) {
