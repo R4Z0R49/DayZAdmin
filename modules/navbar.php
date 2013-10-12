6bc5718cc6fb295b2d37c3a7449343df2d1a1c93
Marcuz <marcuz1111@gmail.com>
2013-10-12 20:48:28 +0200

    Added a divider

--- modules/navbar.php
+++ modules/navbar.php
@@ -85,6 +85,7 @@ $accesslvl = $db->GetOne("SELECT accesslvl FROM users WHERE id = '$user_id'");
 				<li>
 					<a href="admin.php?view=check">Check Items</a>
 				</li>
+				<li class="divider"></li>
 				<li>
 					<a href="admin.php?view=addVehsSQF">Add Vehicles by mission.sqf</a>
 				</li>
